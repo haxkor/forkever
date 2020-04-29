90fa96d95c5dc694afcd8054e481a0d8da590dcb
haxkor <jasper.ruehl@tum.de>
2020-04-29 17:26:03 +0200

    cleanup

--- venvcopy/venv/lib/python3.7/site-packages/pwnlib/adb/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-from pwnlib.adb.adb import *
-from pwnlib.adb.protocol import Client
-from pwnlib.adb.bootimg import BootImage
-from pwnlib.adb.bootloader import BootloaderImage
