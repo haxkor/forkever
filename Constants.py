2db8e3dc70155545f2b55dabd499cf3db0b2c186
haxkor <jasper.ruehl@tum.de>
2020-05-26 13:32:37 +0200

    added GPLv3 License

--- Constants.py
+++ Constants.py
@@ -5,17 +5,29 @@ UPD_FROMPAULA_INSERT = b"\x02"
 CMD_REQUEST = b"\x50"
 CMD_REQUEST_SUCCESS = b"\x51"
 
+from signal import SIGCHLD
+
+SIGNALS_IGNORE = dict([("SIGCHLD", SIGCHLD)])
+from pwn import asm
+
+SYSCALL_INSTR = asm("syscall")
+
+# ------- DONT MODIFY STUFF ABOVE ------  #
+
 hyx_path = "/home/jasper/CLionProjects/hyxWIP/hyx-0.1.5/myhyx"
 
 path_launcher = "launcher/launcher"
-socketname= "forkever_hyx_sock"
+socketname = "/tmp/forkever_hyx_sock"
 
-runargs = None# ["x-terminal-emulator", "--hold", "-e"   ]
+# this will be used to launch hyx, set to "None" and the command will be printed out so you can launch it yourself
+runargs = ["x-terminal-emulator", "--hold", "-e"]
 
+# Breakpoints in PIEs will be relative to the base adress if they are below this value
 RELATIVE_ADRESS_THRESHOLD = 0xFFffFFff
 
 PRINT_BORING_SYSCALLS = True
 
+# if this is true, you need to be careful to not continue on a process that is waiting on another process. It also isnt really tested
 FOLLOW_NEW_PROCS = False
 
 # when looking up a symbol, other symbols matching the lookup will be printed
@@ -23,11 +35,3 @@ PRINT_OTHER_CANDIDATES = False
 
 HOST = ""
 PORT = 9999
-
-from signal import SIGCHLD
-
-SIGNALS_IGNORE = dict([("SIGCHLD", SIGCHLD)])
-
-from pwn import asm
-
-SYSCALL_INSTR = asm("syscall")
