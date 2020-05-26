ba8c3695bd757229fe01137ee2f30f59b428c124
haxkor <jasper.ruehl@tum.de>
2020-05-26 21:13:24 +0200

    adjusted constans

--- Constants.py
+++ Constants.py
@@ -14,19 +14,18 @@ SYSCALL_INSTR = asm("syscall")
 
 # ------- DONT MODIFY STUFF ABOVE ------  #
 
-hyx_path = "/home/jasper/CLionProjects/hyxWIP/hyx-0.1.5/myhyx"
 hyx_path = "../hyx4forkever/hyx"
 
 path_launcher = "launcher/launcher"
 socketname = "/tmp/forkever_hyx_sock"
 
 # this will be used to launch hyx, set to "None" and the command will be printed out so you can launch it yourself
-runargs = ["x-terminal-emulator", "--hold", "-e"]
+runargs = ["x-terminal-emulator", "--hold"]     #, "-e"]
 
 # Breakpoints in PIEs will be relative to the base adress if they are below this value
 RELATIVE_ADRESS_THRESHOLD = 0xFFffFFff
 
-PRINT_BORING_SYSCALLS = True
+PRINT_BORING_SYSCALLS = False
 
 # if this is true, you need to be careful to not continue on a process that is waiting on another process. It also isnt really tested
 FOLLOW_NEW_PROCS = False
