5468ec4cba1201cedbd183a56c24e7bc83917390
haxkor <jasper.ruehl@tum.de>
2020-05-30 17:02:20 +0200

    better exiting and handling of possible outputs of process

--- forkever.py
+++ forkever.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import pwn
+
 pwn.context.log_level = "ERROR"
 
 from argparse import ArgumentParser, REMAINDER
@@ -9,27 +10,30 @@ from ProcessWrapper import LaunchArguments
 
 p = ArgumentParser()
 p.add_argument("-init", help="Pass a file for initial commands")
-p.add_argument("-rand", action="store_true",help="to enable randomization")    # randomization disabled by default
+p.add_argument("-rand", action="store_true", help="to enable randomization")  # randomization disabled by default
 p.add_argument("-sock", action="store_true",
-               help="if you want to communicate with the program via a socket. (Adjust in Constants.py)")    # no socket by default
+               help="if you want to communicate with the program via a socket. (Adjust in Constants.py)")  # no socket by default
 p.add_argument("runargs", nargs=REMAINDER)
 
 parsed_args = p.parse_args()
-#print("runargs= %s" % parsed_args.runargs)
-#print("inputsock = ", parsed_args.sock)
+# print("runargs= %s" % parsed_args.runargs)
+# print("inputsock = ", parsed_args.sock)
 launch_args = LaunchArguments(parsed_args.runargs, parsed_args.rand)
 
 handler = InputHandler(launch_args, startupfile=parsed_args.init, inputsock=parsed_args.sock)
+
 
 def _handle_final_outputs(poll_res):
     from operator import itemgetter
     get_first = itemgetter(0)
     get_outs = lambda poll_elem: get_first(poll_elem)
     outs = map(get_outs, poll_res)
+
     if any("out" in out for out in outs):
         handler.handle_procout(None, None, None)
     if any("err" in out for out in outs):
         handler.handle_stderr(None, None)
+
 
 try:
     handler.inputLoop()
